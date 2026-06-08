89ef95f08ce513640e404253baa18119189feb1a
Nordth <emo-name@hotmail.com>
2026-06-08 19:07:19 +0400

    Fix GraphBlock locales

---
+++ creators/i18n/locales/en/graphBlock.ts
@@ -0,0 +1,14 @@
+export default {
+  graphBlock: {
+    node: {
+      title: 'Node',
+      placeholder: 'Enter text...',
+    },
+    editor: {
+      addNode: 'Add Node',
+    },
+    outline: {
+      rootTitle: 'Graph',
+    },
+  },
+};
---
+++ creators/i18n/locales/ru/graphBlock.ts
@@ -0,0 +1,14 @@
+export default {
+  graphBlock: {
+    node: {
+      title: 'Узел',
+      placeholder: 'Введите текст...',
+    },
+    editor: {
+      addNode: 'Добавить узел',
+    },
+    outline: {
+      rootTitle: 'Граф',
+    },
+  },
+};
