e8989ef1651f4662b05f438ab345a2af7e1dec74
Dreak <dreak@intel-bytes.net>
2026-06-11 20:50:24 +0200

    i18n: add German (de) translation strings, added de to i18n.config.ts

---
+++ desktop/i18n/locales/de.ts
@@ -0,0 +1,19 @@
+import desktop from './de/desktop';
+import index from '~ims-app-base/../i18n/locales/de/index';
+import translatedTitles from '~ims-app-base/../i18n/locales/de/translatedTitles';
+import countries from '~ims-app-base/../i18n/locales/de/countries';
+import imsDialogEditor from "~ims-creators/../i18n/locales/de/imsDialogEditor";
+import graphBlock from "~ims-creators/../i18n/locales/de/graphBlock";
+import creatorsCommon from "~ims-creators/../i18n/locales/de/creatorsCommon";
+import levelEditor from "~ims-creators/../i18n/locales/de/levelEditor";
+
+export default {
+  ...index,
+  ...translatedTitles,
+  ...countries,
+  ...imsDialogEditor,
+  ...graphBlock,
+  ...creatorsCommon,
+  ...levelEditor,
+  ...desktop
+};
