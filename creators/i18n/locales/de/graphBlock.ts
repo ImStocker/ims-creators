28f82cdfbd5d252eb6fd3756b07076be86639fbb
Nordth <emo-name@hotmail.com>
2026-06-25 13:31:09 +0400

    Add german language

---
+++ creators/i18n/locales/de/graphBlock.ts
@@ -0,0 +1,27 @@
+export default {
+  graphBlock: {
+    node: {
+      title: 'Knoten',
+      placeholder: 'Text eingeben...',
+    },
+    editor: {
+      addNode: 'Knoten hinzufügen',
+      addCard: 'Karte hinzufügen',
+      addFile: 'Datei hinzufügen',
+      addElement: 'Element hinzufügen',
+      selectAsset: 'Element auswählen',
+      replaceFile: 'Datei ersetzen',
+      replaceAsset: 'Element ersetzen',
+      deleteNode: 'Knoten löschen',
+      cutNode: 'Ausschneiden',
+      pasteNode: 'Aus Zwischenablage einfügen',
+      noGraphInClipboard: 'Keine Graphdaten in der Zwischenablage',
+      emptyHint: 'Rechtsklick auf das Feld, um einen Knoten zu erstellen.',
+      setServiceName: 'Dienstnamen festlegen',
+      serviceNameAlreadyExists: 'Dienstname existiert bereits',
+    },
+    outline: {
+      rootTitle: 'Graph',
+    },
+  },
+};
