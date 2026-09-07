331662aca96eac53b8f34aa47a3179ce4fb1ce3d
Nordth <emo-name@hotmail.com>
2026-09-07 22:22:38 +0400

    Add missing localization key in locales

--- creators/i18n/locales/de/creatorsCommon.ts
+++ creators/i18n/locales/de/creatorsCommon.ts
@@ -2,4 +2,35 @@ export default {
   creatorsCommon: {
     fitToScreen: 'An Bildschirm anpassen',
   },
+  localization: {
+    language: 'Sprache',
+    status: {
+      new: 'Neu',
+      changed: 'Wert wurde nach Eingabe der Übersetzung geändert',
+      needReview: 'Überprüfung erforderlich',
+      done: 'Erledigt',
+    },
+    statusClickToConfirm: 'Klicken, um die Übersetzung zu bestätigen',
+    table: {
+      key: 'Schlüssel',
+      original: 'Original',
+      translated: 'Übersetzung',
+      status: 'Status',
+      statusShort: 'St',
+    },
+    locales: {
+      'en-US': 'Englisch (USA)',
+      'zh-CN': 'Chinesisch (vereinfacht)',
+      'ru-RU': 'Russisch (Russland)',
+      'fr-FR': 'Französisch (Frankreich)',
+      'es-ES': 'Spanisch (Spanien)',
+      'en-GB': 'Englisch (Großbritannien)',
+      'de-DE': 'Deutsch (Deutschland)',
+      'pt-BR': 'Portugiesisch (Brasilien)',
+      'en-CA': 'Englisch (Kanada)',
+      'es-MX': 'Spanisch (Mexiko)',
+      'it-IT': 'Italienisch (Italien)',
+      'ja-JP': 'Japanisch (Japan)',
+    },
+  },
 };
--- creators/i18n/locales/en/creatorsCommon.ts
+++ creators/i18n/locales/en/creatorsCommon.ts
@@ -2,4 +2,35 @@ export default {
   creatorsCommon: {
     fitToScreen: 'Fit to screen',
   },
+  localization: {
+    language: 'Language',
+    status: {
+      new: 'New',
+      changed: 'Value was changed after translation entered',
+      needReview: 'Need review',
+      done: 'Done',
+    },
+    statusClickToConfirm: 'Click to confirm translation',
+    table: {
+      key: 'Key',
+      original: 'Original',
+      translated: 'Translation',
+      status: 'Status',
+      statusShort: 'St',
+    },
+    locales: {
+      'en-US': 'English (United States)',
+      'zh-CN': 'Chinese (simplified)',
+      'ru-RU': 'Russian (Russia)',
+      'fr-FR': 'French (France)',
+      'es-ES': 'Spanish (Spain)',
+      'en-GB': 'English (United Kingdom)',
+      'de-DE': 'German (Germany)',
+      'pt-BR': 'Portuguese (Brazil)',
+      'en-CA': 'English (Canada)',
+      'es-MX': 'Spanish (Mexico)',
+      'it-IT': 'Italian (Italy)',
+      'ja-JP': 'Japanese (Japan)',
+    },
+  },
 };
